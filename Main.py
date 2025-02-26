06553ec3e530f3989a6877d4022687b9bdfbd747
carlaum167 <carlosgado167@gmail.com>
2025-02-26 14:33:11 -0300

    alterações nas classes -> Main, Conta e Cliente

--- Main.py
+++ Main.py
@@ -1,13 +1,16 @@
 class Main:
     pass
 
-print("testando o projeto")
-
 from Cliente import Cliente
 
 from Conta import Conta
 
-c1= Cliente("João", "114444-2222")
-conta=Conta(c1.nome,6565,0)
 
-print(conta.titular," Numero: ",conta.numero, "Seu Saldo: ", conta.saldo)
+
+c1= Cliente("João", "114444-2222")
+conta=Conta(c1.get_nome(), 1222, 0)
+
+
+conta.deposita(1000)
+conta.saque(50)
+conta.extrato()
